507f344098ad07691c692cb777fbd20635c55840
null-mtrx <w4sp1@proton.me>
2026-06-09 23:07:13 +0530

    feat(speech recognition) Added the ability to convert the speech to text using sherpa
    ## Change Log
    - Added a config.json for easier access to parameters for speech to text and text to speech
    - Added recogniser.py that uses sherpa-onnx to help convert the speech data to text

--- listener/speech_processor.py
+++ listener/speech_processor.py
@@ -6,9 +6,11 @@ import sounddevice
 import numpy as np
 import queue
 
+from .transcriber import Transcriber
+
 # Audio input parameters
-SAMPLE_RATE = 16_000
-BLOCK_SIZE = 4096
+SAMPLE_RATE = 16000
+BLOCK_SIZE = int(0.1 * SAMPLE_RATE)
 CHANNEL_TYPE = 1
 MAX_AUDIO_CHUNKS = 4
 
@@ -18,53 +20,19 @@ SILENCE_BLOCK_COUNT = 7
 
 class ProcessSpeech:
     def __init__(self):
-        self.audio_queue = queue.Queue()
+        self.transcriber = Transcriber()
 
     def process_input_stream(self):
-        audio_stack = []
-        audio_chunk_count = 0
-        speech_started = False
-        silence_block_count = 0
-
-        def callback(indata, frames, time, status):
-            self.audio_queue.put(indata)
-
-        input_stream = sounddevice.RawInputStream(
+        input_stream = sounddevice.InputStream(
             samplerate=SAMPLE_RATE,
             blocksize=BLOCK_SIZE,
             channels=CHANNEL_TYPE,
-            callback=callback,
         )
 
-        with input_stream:
+        print("Speech recognition starting now")
+        with input_stream as stream:
             while True:
-                in_data = self.audio_queue.get()
-                audio_data = np.frombuffer(in_data, dtype=np.float32)
-                audio_rms = np.sqrt(np.mean(audio_data**2))
+                audio_data, _ = stream.read(BLOCK_SIZE)
+                audio_data = audio_data.reshape(-1)
 
-                # for debugging purposes
-                print("RMS value", audio_rms)
-                print("Silent Block Count", silence_block_count)
-
-                if audio_rms > SILENCE_RMS_THRESHOLD:
-                    silence_block_count = 0
-                    if not speech_started:
-                        speech_started = True
-                else:
-                    if speech_started:
-                        silence_block_count += 1
-
-                audio_stack.append(audio_data)
-                audio_chunk_count += 1
-
-                if audio_chunk_count == MAX_AUDIO_CHUNKS:
-                    audio_chunk = np.concatenate(audio_stack)
-                    print(audio_chunk.shape)
-
-                    for i in range(3):
-                        audio_stack.pop(0)
-
-                    audio_chunk_count = 1
-
-                if silence_block_count >= SILENCE_BLOCK_COUNT:
-                    break
+                self.transcriber.speech_to_text(audio_data, sample_rate=SAMPLE_RATE)
---
+++ listener/transcriber.py
@@ -0,0 +1,59 @@
+"""
+This module is responsible for transcribing the speech data into text
+"""
+
+import sherpa_onnx
+import pathlib
+import json
+
+NUM_THREADS = 2
+SAMPLE_RATE = 16000
+FEATURE_DIM = 80
+
+RULE1_MIN_TRAILING_SILENCE = 2.4
+RULE2_MIN_TRAILING_SILENCE = 1.2
+RULE3_MIN_UTTERANCE_LENGTH = 300
+
+with open("/home/w4sp/Projects/Voice-Assistant/src/config.json", "r") as file:
+    config = json.load(file)
+
+
+class Transcriber:
+    def __init__(self):
+        self.tokens = config["listener_params"]["tokens"]
+        self.encoder = config["listener_params"]["encoder_path"]
+        self.decoder = config["listener_params"]["decoder_path"]
+        self.joiner = config["listener_params"]["joiner_path"]
+
+        self.recogniser = sherpa_onnx.OnlineRecognizer.from_transducer(
+            tokens=self.tokens,
+            encoder=self.encoder,
+            decoder=self.decoder,
+            joiner=self.joiner,
+            num_threads=NUM_THREADS,
+            sample_rate=SAMPLE_RATE,
+            feature_dim=FEATURE_DIM,
+            rule1_min_trailing_silence=RULE1_MIN_TRAILING_SILENCE,
+            rule2_min_trailing_silence=RULE2_MIN_TRAILING_SILENCE,
+            rule3_min_utterance_length=RULE3_MIN_UTTERANCE_LENGTH,
+            decoding_method="greedy_search",
+            provider="cpu",
+        )
+        self.stream = self.recogniser.create_stream()
+        self.display = sherpa_onnx.Display()
+
+    def speech_to_text(self, data, sample_rate):
+        self.stream.accept_waveform(sample_rate, data)
+
+        while self.recogniser.is_ready(self.stream):
+            self.recogniser.decode_stream(self.stream)
+
+        result = self.recogniser.get_result(self.stream)
+        end_of_speech = self.recogniser.is_endpoint(self.stream)
+
+        print(result, end="")
+        if end_of_speech:
+            if result:
+                print(result)
+
+            self.recogniser.reset(self.stream)
