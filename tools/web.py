d7e9e744fc54e00329127235e4147d4d0283a2bd
null-mtrx <w4sp1@proton.me>
2026-08-09 21:19:31 +0530

    feat(youtube video search) Allows the agent to open the video based on the user's search query

--- tools/web.py
+++ tools/web.py
@@ -2,6 +2,7 @@ from langchain_core.tools import tool
 
 import webbrowser
 import wikipedia
+import youtubesearchpython
 
 
 @tool
@@ -14,3 +15,11 @@ def open_webpage(web_page_url: str) -> None:
 def open_wiki_page(wiki_title: str) -> str:
     """Given a topic on wikipedia, returns its summary"""
     return wikipedia.summary(wiki_title)
+
+
+@tool
+def open_youtube_video(title: str):
+    """Based on the video title/search query, it returns the url of the first video"""
+    query_search = youtubesearchpython.VideosSearch(title, limit=1)
+    url = query_search.result()["result"]["link"]
+    webbrowser.open(url)
