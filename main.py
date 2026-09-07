3c85e5e0b502db22eee422b85c8d6e6af82609a3
null-mtrx <w4sp1@proton.me>
2026-09-07 19:14:59 +0530

    style(main.py) Removed some unnecessary comments in main.py

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-"""
-import json
-from listener.speech_processor import ProcessSpeech
-
-with open("/home/w4sp/Projects/Voice-Assistant/config.json", "r") as file:
-    config = json.load(file)
-
-ProcessSpeech(config).process_input_stream()
-"""
-
 from PySide6.QtWidgets import QApplication
 from window import MainWindow
 import sys
